09a62f119241ae3fc8bfeaaece763cb75818745a
SCM <mf5074111@gmail.com>
2023-08-10 14:05:52 +0500

    Ist commit

---
+++ app/src/main/java/com/example/wavesoffood/SplashScreen.kt
@@ -0,0 +1,18 @@
+package com.example.wavesoffood
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+@Suppress("DEPRECATION")
+class SplashScreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash_screen)
+        Handler().postDelayed({
+            val intent= Intent(this,MainActivity::class.java)
+            startActivity(intent)
+        },3000)
+    }
+}
