1aa70b04a3a821a2f14582b168f1742b78885ade
SCM <mf5074111@gmail.com>
2023-08-11 12:41:32 +0500

    Ist commit

--- app/src/main/java/com/example/wavesoffood/LoginActivity.kt
+++ app/src/main/java/com/example/wavesoffood/LoginActivity.kt
@@ -1,11 +1,24 @@
 package com.example.wavesoffood
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.wavesoffood.databinding.ActivityLoginBinding
 
 class LoginActivity : AppCompatActivity() {
+    private val binding:ActivityLoginBinding by lazy {
+        ActivityLoginBinding.inflate(layoutInflater)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+        setContentView(binding.root)
+        binding.loginButton.setOnClickListener {
+            val intent=Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+        }
+        binding.donthaveButton.setOnClickListener {
+            val intent=Intent(this,SignUpActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
---
+++ app/src/main/java/com/example/wavesoffood/SignUpActivity.kt
@@ -0,0 +1,21 @@
+package com.example.wavesoffood
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.wavesoffood.databinding.ActivitySignUpBinding
+
+class SignUpActivity : AppCompatActivity() {
+    private val binding:ActivitySignUpBinding by lazy {
+        ActivitySignUpBinding.inflate(layoutInflater)
+    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+
+        binding.alreadyaccountButton.setOnClickListener {
+            val intent= Intent(this,LoginActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
