8d9428ba5dfdb590c8cdda5f7da0cfaae868b51e
SCM <mf5074111@gmail.com>
2023-08-21 23:07:11 +0500

    Ist commit

--- app/src/main/java/com/example/wavesoffood/Fragments/HomeFragment.kt
+++ app/src/main/java/com/example/wavesoffood/Fragments/HomeFragment.kt
@@ -5,10 +5,16 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import com.denzcoskun.imageslider.constants.ScaleTypes
+import com.denzcoskun.imageslider.interfaces.ItemClickListener
+import com.denzcoskun.imageslider.models.SlideModel
 import com.example.wavesoffood.R
+import com.example.wavesoffood.databinding.FragmentHomeBinding
 
 
 class HomeFragment : Fragment() {
+    private lateinit var binding:FragmentHomeBinding
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -21,7 +27,32 @@ class HomeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        binding=FragmentHomeBinding.inflate(layoutInflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val imageList=ArrayList<SlideModel>()
+        imageList.add(SlideModel(R.drawable.banner1,ScaleTypes.FIT))
+        imageList.add(SlideModel(R.drawable.banner2,ScaleTypes.FIT))
+        imageList.add(SlideModel(R.drawable.banner3,ScaleTypes.FIT))
+
+        val imageSlider=binding.imageSlider
+        imageSlider.setImageList(imageList)
+        imageSlider.setImageList(imageList,ScaleTypes.FIT)
+        imageSlider.setItemClickListener(object :ItemClickListener{
+            override fun doubleClick(position: Int) {
+                TODO("Not yet implemented")
+            }
+
+            override fun onItemSelected(position: Int) {
+                val itemPosition=imageList[position]
+                val itemMessage="Selected Image $position"
+                Toast.makeText(requireContext(), itemMessage, Toast.LENGTH_SHORT).show()
+            }
+
+        })
     }
 
     companion object {
